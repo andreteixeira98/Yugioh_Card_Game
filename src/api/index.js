06666e3a5232911fe66223b355845a760ad04ca2
andreteixeira98 <andreteixeiracbjr@gmail.com>
2025-09-28 22:41:02 -0300

    get form api

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,23 @@
-async function fetchData(url) {
+export async function fetchData(url) {
     const response = await fetch(url);
     const data = await response.json();
     return data;
 }
 
-async function getCardByName(cardName) {
-    const url = `https://api.yugioh.com/cards?name=${cardName}`;
+export async function getCardByName(cardName) {
+    const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${cardName}`;
     const data = await fetchData(url);
     return data;
 }
 
-async function getAllCards() {
-    const url = `https://api.yugioh.com/cards`;
+export  async function getAllCards() {
+    const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php`;
     const data = await fetchData(url);
     return data;
 }
 
-async function getCardsByArchetype(archetype) {
-    const url = `https://api.yugioh.com/cards?archetype=${archetype}`;
+export async function getCardsByArchetype(archetype) {
+    const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=${archetype}`;
     const data = await fetchData(url);
     return data;
 }
